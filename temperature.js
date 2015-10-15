53c0d2b32e6d925ced94f43f274f47bf98372a92
alu0100406122 <alu0100406122@ull.edu.es>
2015-10-15 16:54:05 +0000

    cambiando .js

--- temperature.js
+++ temperature.js
@@ -11,16 +11,26 @@ function temperatura(valor, tipo){
 }
 
 temperatura.prototype = new medida();
-/*
+
+medida.prototype.set_valor = function(){
+    this.valor=valor;
+}
+
+medida.prototype.set_tipo = function(){
+    this.tipo=tipo;
+}
+
 medida.prototype.get_valor = function(){
     return this.valor=valor;
 }
-medida.prototype.get_valor = function(){
-    return this.valor=valor;
+
+medida.prototype.get_tipo = function(){
+    return this.tipo=tipo;
 }
-*/
+
 
 function calculate(){
+    
     var result = new temperatura();
     
     var original = document.getElementById("original");
@@ -30,16 +40,19 @@ function calculate(){
     var m = temp.match(regexp);     /*Cuando cumple la expresión regular*/
     
     if (m){
-        var num = m[1];
+        /*var num = m[1];
         var type = m[2];
-        num = parseFloat(num);      //string --> float
+        num = parseFloat(num);      //string --> float*/
 
-        if (type=='c' || type=='C'){
-            result = (num * 9/5)+32;
+        result.set_valor(parseFloat(m[1]));
+        result.set_valor(m[2]);
+
+        if (result.get_tipo=='c' || result.get_tipo=='C'){
+            result.set_valor((num * 9/5)+32);
             result = result.toFixed(2) + " Farenheit";  //toFixed --> Redondeo
         }
         else{
-            result = (num -32)*5/9;
+            result.set_valor((num -32)*5/9);
             result = result.toFixed(2) + " Celsius";
         }
         converted.innerHTML = result;
